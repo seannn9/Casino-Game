be655b17389469d7ecfb18a967dd2fe089dd55c6
seannn9 <naeskirlu9@gmail.com>
2023-04-06 01:23:33 +0800

    Created function templates

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <random>
 using namespace std;
 
+float betAmount() {
+    float bet;
+    cout << "Input bet amount: ";
+    cin >> bet;
+    return bet;
+}
+
+void guess(float bet) {
+    while (bet > 0) {
+        srand(time(0));
+        randNum = rand() % 10 + 1;
+    }
+}
+
 int main() {
-    cout << "Hello World";
+    float bet = betAmount();
+    guess(bet);
     return 0;
 }
